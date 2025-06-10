0cd60b129aeae37e1d5bb771eb8b6574d4e7b952
oshobugie <soshobugie@gmail.com>
2025-06-10 14:16:55 +0100

    update

--- websitescript.py
+++ websitescript.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-# Author: Iyke
+# Author: Sam
 # Date Created: 17th May 2025
-# Last Modified: 18th May 2025
+# Last Modified: 10th June 2025
 # Description: This Script takes in user inputs
 # Usage: Change the file permission to executable and run with (./)
 
